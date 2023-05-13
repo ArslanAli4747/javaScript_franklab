d09f991c21b326935eeadc8c47af93293e947ce5
ArslanAli4747 <105070803+ArslanAli4747@users.noreply.github.com>
2023-05-13 20:04:33 +0500

    ADDED

--- pixel_effect/app.js
+++ pixel_effect/app.js
@@ -5,28 +5,44 @@ canva.height = window.innerHeight ;
 
 class Particle{
     constructor(effect,color,x,y){
-        this.x = x;
-        this.y = y;
-        this.size = 2;
-        this.vx = Math.random()*2 - 1;
-        this.vy = Math.random()*2 - 1;
+        this.effect = effect;
+        this.x = Math.random()*this.effect.width;
+        this.y = Math.random()*this.effect.height;
+        this.size = 3;
+        this.vx = 0;
+        this.vy = 0;
         this.originX = Math.floor(x);
         this.originY = Math.floor(y);
         this.color = color
-        this.speed = Math.floor(Math.random()*10);
-        this.check = 0;
+        this.ease = 0.05;
+        this.angle = 0;
+        this.dx = 0;
+        this.dy=0;
+        this.fraction = 0.8;
+        this.distance = 0;
+        this.force = 0;
+
+
+        
+     
     }
     draw(cx){
         cx.fillStyle = this.color
         cx.fillRect(this.x,this.y,this.size,this.size)
     }
     update(){
-        this.check++;
-        if (this.check>this.speed){
-            this.x+=this.vx;
-            this.y+=this.vy;
-            this.check = 0;
-        }
+            this.dx = this.effect.mouse.x - this.x;
+            this.dy = this.effect.mouse.y - this.y;
+            this.distance = this.dx * this.dx + this.dy * this.dy;
+            this.force = -(this.effect.mouse.radius )/ this.distance;
+            if (this.distance < this.effect.mouse.radius){
+                this.angle = Math.atan2(this.dy,this.dx);
+                this.vx += this.force * Math.cos(this.angle);
+                this.vy += this.force * Math.sin(this.angle);
+            }
+
+            this.x += (this.vx*=this.fraction) +(this.originX - this.x) * this.ease;
+            this.y += (this.vy*=this.fraction) +(this.originY - this.y) * this.ease;
         
     }
 }
@@ -40,13 +56,23 @@ class Effect{
         this.centerY = this.height * 0.5;
         this.cx = this.centerX - this.image.width * 0.5;
         this.cy = this.centerY - this.image.height * 0.5;
-        this.gap = 5;
+        this.gap = 3;
+        this.mouse={
+            radius:3000,
+            x:undefined,
+            y:undefined 
+        }
+        addEventListener('mousemove',event=>{
+            this.mouse.x = event.clientX;
+            this.mouse.y = event.clientY;
+            // console.log(this.mouse.x,this.mouse.y);
+        });
     }
     init(cx){
         cx.drawImage(this.image,this.cx,this.cy);
         
         const imageData = cx.getImageData(0,0,this.width,this.height).data;
-        console.log(imageData);
+     
         for (let y = 0; y < this.height; y+=this.gap) {
             for (let x = 0; x < this.width; x+=this.gap) {
                 const index = (y * this.width + x) * 4;
